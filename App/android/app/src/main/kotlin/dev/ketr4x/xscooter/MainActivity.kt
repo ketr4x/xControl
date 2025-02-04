b90eaf690a6d69e863a5beb42d1d563004abab01
ketr4x <ketraxpl@gmail.com>
2025-02-04 16:25:15 +0100

    Change of the project's structure

--- App/android/app/src/main/kotlin/dev/ketr4x/xscooter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package dev.ketr4x.xscooter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ xControl Utility/android/app/src/main/kotlin/dev/ketr4x/xcontrol_utility/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.ketr4x.xcontrol_utility
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
